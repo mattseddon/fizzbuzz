c398b356557e198b1e41cc07b9aba5783f192b9e
Matt Seddon <mattseddon@hotmail.com>
2025-02-26 22:09:04 +1100

    use match instead of if else

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,11 @@ struct Cli {
 }
 
 fn fizz_buzz(n: i64) -> String {
-    if n % 3 == 0 && n % 5 == 0 {
-        "FizzBuzz".to_string()
-    } else if n % 3 == 0 {
-        "Fizz".to_string()
-    } else if n % 5 == 0 {
-        "Buzz".to_string()
-    } else {
-        n.to_string()
+    match (n % 3 == 0, n % 5 == 0) {
+        (true, true) => "FizzBuzz".to_string(),
+        (true, false) => "Fizz".to_string(),
+        (false, true) => "Buzz".to_string(),
+        (false, false) => n.to_string(),
     }
 }
 
